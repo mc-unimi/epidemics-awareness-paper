a953aec04a15568cc21ef8836d2d0e06fe4c36ab
mc-unimi <47218159+mc-unimi@users.noreply.github.com>
2019-09-26 16:19:39 +0200

    Update customexceptions.py

--- code/customexceptions.py
+++ code/customexceptions.py
@@ -1,3 +1,21 @@
+#Copyright (C) 2019 Samira Maghool and Marco Cremonini
+#
+#This file is part of the multiagent epidemic simulator developed by the research project described in the paper titled "A multicomponent model of awareness for different categories of network epidemics", by Samira Maghool, Nahid Maleki-Jirsaraei, and Marco Cremonini.
+#
+#    The multiagent epidemic simulator is free software: you can  
+#    redistribute it and/or modify it under the terms of the GNU General
+#    Public License as published by the Free Software Foundation, either
+#    version 3 of the License, or (at your option) any later version.
+#
+#    The multiagent epidemic simulator is distributed in the hope that it 
+#    will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+#    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+#    See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with the multiagent epidemic simulator.  
+#    If not, see <https://www.gnu.org/licenses/>.
+
 '''
 Custom exceptions when handling files to be more precise about the error
 that occurred. 
