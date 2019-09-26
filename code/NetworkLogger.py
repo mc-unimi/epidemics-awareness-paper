9e15d2cdbfd05fc502fd6978b6ba66ec6b5aef5a
mc-unimi <47218159+mc-unimi@users.noreply.github.com>
2019-09-26 16:18:00 +0200

    Update NetworkLogger.py

--- code/NetworkLogger.py
+++ code/NetworkLogger.py
@@ -1,3 +1,20 @@
+#Copyright (C) 2019 Samira Maghool and Marco Cremonini
+#
+#This file is part of the multiagent epidemic simulator developed by the research project described in the paper titled "A multicomponent model of awareness for different categories of network epidemics", by Samira Maghool, Nahid Maleki-Jirsaraei, and Marco Cremonini.
+#
+#    The multiagent epidemic simulator is free software: you can  
+#    redistribute it and/or modify it under the terms of the GNU General
+#    Public License as published by the Free Software Foundation, either
+#    version 3 of the License, or (at your option) any later version.
+#
+#    The multiagent epidemic simulator is distributed in the hope that it 
+#    will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+#    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+#    See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with the multiagent epidemic simulator.  
+#    If not, see <https://www.gnu.org/licenses/>.
 
 ''' This module is for logging node attributes in the agent-based simulation'''
 ''' @author: samiramaghool@gmail.com>'''
